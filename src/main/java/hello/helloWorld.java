ff40fc637f8d2604f60d36cd458606a6aa3e4ee0
training_B8C.03.01 <training_B8C.03.01@accenture.com>
2017-08-31 12:17:44 +0530

    hello world Project added

---
+++ src/main/java/hello/helloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+		System.out.println("Added newly");
+		System.out.println("Added newly");
+		System.out.println("Added newly");
+    }
+}
---
+++ src/test/java/hello/helloTest.java
@@ -0,0 +1,14 @@
+package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class helloTest {
+
+	@Test
+	public void TestMethod1() {
+		assertEquals(10, 10);
+	}
+
+}
